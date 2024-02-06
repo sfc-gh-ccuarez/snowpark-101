7ec905078973e113b1e01325786950d006d69e1e
Carlos Suarez <smca310777@gmail.com>
2024-02-06 00:02:14 -0600

    New update

---
+++ credenciales.py
@@ -0,0 +1,45 @@
+'''
+connection_parameters = {
+    "account": "",
+    "user": "",
+    "password": "",
+    "warehouse": "python_wh",
+    "role": "devrole",
+    "database": "SP2023",
+    "schema": "public"
+}
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
